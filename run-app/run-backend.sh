f6fc89c3fd879e88ce4e805a34a05f46cd03ffe0
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2021-11-04 12:11:07 -0500

    Working app!

--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -3,10 +3,14 @@ echo "Hello from backend!"
 
 # Store bin in different folder to avoid bind mounts to manipulate it and F* up.
 rm -rf /app
-mkdir /app
+mkdir -p /app
 cp /shared/server/vuego-demoapp /app
 cd /app
+
+# Export env var
+sudo echo "export PORT=$PORT" >> /etc/profile
 # Run server. Append & to start as backgrounded
+echo $PORT
 ./vuego-demoapp &
 
 # TODO: Create service using systemd and enable it.
