38f23df7c897161c0bd457701244fe19770454ca
ana-os-mo <anmosoriomo@unal.edu.co>
2021-11-04 05:21:06 -0500

    git-go-node

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,2 +1,43 @@
 #!/usr/bin/env bash
-echo "Hello world!"
+
+# verifica si esta instalado
+# si lo esta no lo instala
+if ! command -v git &> /dev/null
+then
+    sudo yum install -y git
+fi
+git --version
+
+# ----------------------------------------------
+
+# descarga, instala y agrega al path
+if [[ ! -f go1.17.2.linux-amd64.tar.gz ]]
+then
+    wget -c https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
+    sudo sh -c "echo export PATH=$PATH:/usr/local/go/bin >> /etc/profile"
+    source /etc/profile
+else
+    sudo rm -rf /usr/local/go
+    sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
+    sudo sh -c "echo export PATH=$PATH:/usr/local/go/bin >> /etc/profile"
+    source /etc/profile
+fi
+go version
+
+# ----------------------------------------------
+
+if [[ ! -f node-v16.13.0-linux-x64.tar.gz ]]
+then
+    wget -c https://nodejs.org/download/release/latest-gallium/node-v16.13.0-linux-x64.tar.gz
+    sudo tar -C /bin -xzf node-v16.13.0-linux-x64.tar.gz
+    sudo sh -c "echo export PATH=$PATH:/bin/node-v16.13.0-linux-x64/bin >> /etc/profile"
+    source /etc/profile
+else
+    sudo rm -rf /bin/node-v16.13.0-linux-x64
+    sudo tar -C /bin -xzf node-v16.13.0-linux-x64.tar.gz
+    sudo sh -c "echo export PATH=$PATH:/bin/node-v16.13.0-linux-x64/bin >> /etc/profile"
+    source /etc/profile
+fi
+node --version
+npm --version
